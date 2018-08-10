e082d22735ddee1461be11872178dd8c2ea3962f
Andrew Carpenter <andrew@audioinhd.com>
2018-08-10 19:28:46 -0400

    Ensure versions are sorted in correct order

--- index.js
+++ index.js
@@ -19,20 +19,35 @@ module.exports = goVersions
 // Main
 function goVersions () {
   return new Promise((resolve, reject) => {
-    let relRgx = new RegExp(/^go(\d+\.\d+\.\d+)/)
     https.get(gtagOpts, (res) => {
       let json = ''
-      let tags = {}
       res.on('error', reject)
       res.on('data', (data) => (json += data.toString()))
       res.on('end', () => {
-        json = JSON.parse(json)
-        let rels = json.map((r) => r.ref.replace('refs/tags/', ''))
-        rels.forEach((r) => {
-          tags[(((r || '').match(relRgx) || [])[1])] = true
-        })
-        rels = Object.keys(tags).filter((r) => r && r !== 'undefined')
-        rels = rels.sort().reverse()
+        let tags = []
+        try {
+          json = JSON.parse(json)
+        } catch (ex) {
+          return reject(ex)
+        }
+        for (let entry of json) {
+          let vStr = entry.ref
+          if (vStr.indexOf('refs/tags/go') !== 0) continue
+          vStr = vStr.substr(12)
+          let svNums = vStr.split('.').map((n) => +n)
+          if (svNums[0] !== undefined && Number.isNaN(svNums[0])) continue
+          if (svNums[1] !== undefined && Number.isNaN(svNums[1])) continue
+          if (svNums[2] !== undefined && Number.isNaN(svNums[2])) continue
+          let major = svNums[0] || 0
+          let minor = svNums[1] || 0
+          let patch = svNums[2] || 0
+          tags.push({vStr, major, minor, patch})
+        }
+        let rels = tags.sort((a, b) => {
+          let aScore = (a.major * 10000) + (a.minor * 1000) + a.patch
+          let bScore = (b.major * 10000) + (b.minor * 1000) + b.patch
+          return bScore - aScore
+        }).map((tag) => tag.vStr)
         return resolve(rels)
       })
     }).on('error', reject)
--- test.js
+++ test.js
@@ -2,8 +2,20 @@
 
 import test from 'ava'
 import goVersions from './index'
+import { compare } from 'semver'
 
 test.serial(`should get array of go releases`, async (assert) => {
   let versions = await goVersions()
   assert.true(Array.isArray(versions))
+})
+
+test.serial(`releases should be sorted in semver order`, async (assert) => {
+  let versions = await goVersions()
+  let makeSemver = (v) => {
+    let [major = 0, minor = 0, patch = 0] = v.split('.')
+    return `${major}.${minor}.${patch}`
+  }
+  versions = versions.map(makeSemver)
+  let svSorted = versions.map(makeSemver).sort(compare).reverse()
+  assert.deepEqual(versions, svSorted)
 })
